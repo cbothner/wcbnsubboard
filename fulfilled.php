cf0278d4d0880cc432ae7b1b1e4e2faffcd8bfcb
Cameron Bothner <cbothner@umich.edu>
2013-07-05 11:32:09 -0400

    Show fulfilled requests from later today also.

--- fulfilled.php
+++ fulfilled.php
@@ -19,7 +19,7 @@ $conn;
 $conn = pg_connect("host=$host dbname=$db user=$user password=$pass");
 if(!$conn){die("Database is fail!");}
 
-$query = "SELECT id,sub_name,sub_phone,sub_email,regular_host,show_name,to_char(show_date,'Day DD Month') as show_date_t,to_char(show_start,'HH12:MIam') as show_start_t,to_char(show_end,'HH12:MIam') as show_end_t,comment FROM subs WHERE show_date > NOW()::date AND taken = 'True' ORDER BY show_date ASC,show_start ASC;";
+$query = "SELECT id,sub_name,sub_phone,sub_email,regular_host,show_name,to_char(show_date,'Day DD Month') as show_date_t,to_char(show_start,'HH12:MIam') as show_start_t,to_char(show_end,'HH12:MIam') as show_end_t,comment FROM subs WHERE (show_date > NOW()::date OR (show_date = NOW()::date AND show_start > NOW()::time) ) AND taken = 'True' ORDER BY show_date ASC,show_start ASC;";
 $slotsResource = pg_query($conn, $query);
 pg_close($conn);
 print_r($slotsObject);
