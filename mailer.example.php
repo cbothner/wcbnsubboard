f31009ebc8e05950802f5c9c2a29998447b67980
Cameron Bothner <cbothner@umich.edu>
2014-07-14 20:49:05 -0400

    Fixed the whitespace in the plain text email.

--- mailer.example.php
+++ mailer.example.php
@@ -1,5 +1,5 @@
 <?php
-function sendanemail($subject, $body, $recipient = "YOUR MAILING LIST", $debugging = 0) {
+function sendanemail($subject, $body, $recipient = "rfaa-mod@umich.edu", $debugging = 0) {
     require_once('class.phpmailer.php');
     $mail = new PHPMailer();
     $mail->IsSMTP(); // enable SMTP
@@ -7,14 +7,18 @@ function sendanemail($subject, $body, $recipient = "YOUR MAILING LIST", $debuggi
     $mail->SMTPAuth = true; // authentication enabled
     $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
     $mail->Host = "smtp.gmail.com";
+
     $mail->Port = 465; // or 587
     $mail->IsHTML(true);
     $mail->Username = "YOUR GMAIL ACCOUNT";
     $mail->Password = "YOUR PASSWORD";
     $mail->SetFrom('YOUR EMAIL ADDRESS','YOUR NAME');
-    $mail->Subject = $subject;
+    $mail->Subject = preg_replace('/ +/',' ',$subject);
     $mail->Body = $body;
-    $mail->AltBody = strip_tags($body);
+    $plaintext = trim(preg_replace('/ +/',' ',$body));
+    $plaintext = preg_replace('/&mdash;/','---',$plaintext);
+    $plaintext = strip_tags($plaintext);
+    $mail->AltBody = $plaintext;
     $mail->AddAddress($recipient);
     if(!$mail->Send())
     {
